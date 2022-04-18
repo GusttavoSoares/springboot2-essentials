555610a43a1c6ef97b8d2954a47b8db2e53ce95a
Gustavo Soares <gustavosoares335@yahoo.com.br>
2022-04-18 07:33:04 -0300

    Fazendo GetMapping da home e do /{id}, mudando application.properties para application.yml

--- src/main/java/br/com/ifsp/springboot2/service/AnimeService.java
+++ src/main/java/br/com/ifsp/springboot2/service/AnimeService.java
@@ -1,14 +1,24 @@
 package br.com.ifsp.springboot2.service;
 
 import br.com.ifsp.springboot2.domain.Anime;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
 @Service
 public class AnimeService { // classe responsável pelas regras de negócio
+    private List<Anime> animes = List.of(new Anime(1L, "Boku No Hero"), new Anime(2L, "Mirai Nikki"));
     // private final AnimeRepository animeRepository;
     public List<Anime> listAll() {
-        return List.of(new Anime(1L, "Boku No Hero"), new Anime(2L, "Mirai Nikki"));
+        return animes;
+    }
+
+    public Anime findById(long id) {
+        return animes.stream()
+                .filter(anime -> anime.getId().equals(id))
+                .findFirst()
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Anime not Found"));
     }
 }
