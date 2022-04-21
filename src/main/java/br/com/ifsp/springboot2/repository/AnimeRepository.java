abde3d916a31a19d5c07a3dca4f9a598fe587921
Gustavo Soares <gustavosoares335@yahoo.com.br>
2022-04-21 12:37:50 -0300

    Integrando as classes e pegando os dados diretamente do banco de dados.

--- src/main/java/br/com/ifsp/springboot2/repository/AnimeRepository.java
+++ src/main/java/br/com/ifsp/springboot2/repository/AnimeRepository.java
@@ -1,9 +1,10 @@
 package br.com.ifsp.springboot2.repository;
 
 import br.com.ifsp.springboot2.domain.Anime;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+// vai ser a conexão com o banco de dados
+public interface AnimeRepository extends JpaRepository<Anime, Long> { // id é do tipo Long
 
-public interface AnimeRepository { // vai ser a conexão com o banco de dados
-    List<Anime> listAll();
 }
---
+++ src/main/java/br/com/ifsp/springboot2/requests/AnimePostRequestBody.java
@@ -0,0 +1,9 @@
+package br.com.ifsp.springboot2.requests;
+
+
+import lombok.Data;
+
+@Data
+public class AnimePostRequestBody {
+    private String name;
+}
