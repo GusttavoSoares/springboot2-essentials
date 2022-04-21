d18b5b25586d76a5ca08add9096c4ac10b2137fe
Gustavo Soares <gustavosoares335@yahoo.com.br>
2022-04-21 15:40:32 -0300

    Fazendo a validação dos campos com o javax.validation.

--- src/main/java/br/com/ifsp/springboot2/requests/AnimePostRequestBody.java
+++ src/main/java/br/com/ifsp/springboot2/requests/AnimePostRequestBody.java
@@ -1,9 +1,11 @@
 package br.com.ifsp.springboot2.requests;
 
-
 import lombok.Data;
+
+import javax.validation.constraints.NotEmpty;
 
 @Data
 public class AnimePostRequestBody {
+    @NotEmpty (message = "The anime name cannot be empty")
     private String name;
 }
